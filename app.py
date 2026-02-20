8430762f48b69b6ef9cada38227e78496b73d138
Faithchepchumba <fchepchumba39@gmail.com>
2026-02-20 09:32:40 +0300

    my first commit

---
+++ app.py
@@ -0,0 +1,62 @@
+from flask import *
+import pymysql
+
+
+app= Flask (__name__)
+
+@app.route("/api/signup",methods =["POST"])
+def signup():
+    #code to execute
+    username = request.form['username']
+    email = request.form['email']
+    phone = request.form['phone']
+    password = request.form['password']
+
+    print(username, email, phone, password)
+    # create db connection
+    connection = pymysql.connect(host="localhost",user ="root",password="",database ="faith_sokogarden")
+
+    # create cursor 
+    cursor = connection.cursor(pymysql.cursors.DictCursor)
+
+    # create sql query
+    sql = "insert into users(username,email,phone,password) values (%s,%s,%s,%s)"
+    data =(username, email, phone, password)
+    # execute the query using cursor
+    cursor.execute(sql, data)
+
+    # save the data
+    connection.commit()
+
+    # return response
+    return jsonify({"message":"Sign up api"})
+
+# signin
+@app.route("/api/signin", methods =["POST"])
+def signin():
+    email = request.form['email']
+    password = request.form['password']
+    print(email, password)
+    connection = pymysql.connect(host ="localhost",user="root",password="",database ="faith_sokogarden")
+    cursor = connection.cursor()
+    sql="select user_id,username,email,phone from users where email=%s and password =%s"
+    data =(email, password)
+    cursor.execute(sql,data)
+
+
+    if cursor.rowcount ==0:
+        return jsonify({"message":"invalid credentials"})
+    else:
+        user =cursor.fetchone()
+        return jsonify({"message":"login successful","user":user})
+    
+    
+
+
+
+ 
+
+
+
+if __name__=="__main__":
+    app.run(debug = True)
