7be31ab011e22715bf719e3fb8df5a245ddcead0
Faithchepchumba <fchepchumba39@gmail.com>
2026-02-20 11:09:05 +0300

    insert image in images

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import *
 import pymysql
+import os
 
 
 app= Flask (__name__)
+app.config['UPLOAD_FOLDER']='static/images'
 
 @app.route("/api/signup",methods =["POST"])
 def signup():
@@ -52,10 +54,41 @@ def signin():
     
     
 
+@app.route("/api/add_product", methods =["POST"])
+def addProduct():
+    product_name=request.form['product_name']
+    product_description =request.form['product_description']
+    product_cost=request.form['product_cost']
+    product_category =request.form['product_category']
+    product_image=request.files['product_image']
 
+    
+    print(product_name,product_description,product_cost,product_category,product_image)
 
+    # get image name
+    image_name =product_image.filename
+    print(image_name)
+
+    # save image to images folder
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], image_name)
+    print(file_path)
+    product_image.save(file_path)
  
+    # create a connection db
+    connection=pymysql.connect(host="localhost",user="root",password="",database="faith_sokogarden")
 
+    cursor=connection.cursor()
+    # sql to execute
+    sql="insert into product_details(product_name,product_description,product_cost,product_category,product_image) values (%s,%s,%s,%s,%s)"
+    data =(product_name,product_description,product_cost,product_category,image_name)
+     
+     # execute query
+    cursor.execute(sql,data)
+
+    # save data
+    connection.commit()
+
+    return jsonify({"message":"product added successfully"})
 
 
 if __name__=="__main__":
